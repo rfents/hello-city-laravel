a01d900b3d579322ce6f06b1efb38a6c5fcafbf7
Chef SMPI <fenontsoa@gmail.com>
2021-02-23 21:03:33 +0300

    update readme and home page

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Artisan::command('toto', function () {
+    $this->comment('toto est cool');
+})->purpose('Notre commande toto');
