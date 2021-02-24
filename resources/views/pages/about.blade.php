506499ce2c65cb701a3a253136ef06b559da4f76
Chef SMPI <fenontsoa@gmail.com>
2021-02-24 14:31:28 +0300

    removing duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About | Hello Ciy</title>
-    </head>
-    <body>
-        <h1>Page à propos</h1>
+@section('content')
 
         <p>Exercices de test &hearts;</p>
 
         <h4><a href="/">Revenir à la page d'accueil</a></h4>
 
-    </body>
-    <footer>&copy; Copyright {{ date('Y') }} &middot; </footer>
-</html>
+@endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Hello Ciy</title>
-    </head>
+@section('content')
         <h1>Hello from Madagascar!</h1>
 
         <p>It's actually {{ date('h:i A') }}.</p>
-
-    <footer>&copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About us</a></footer>
-    </body>
-</html>
+@endsection
